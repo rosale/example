fd11e58c8339ec0c01be6ac556f56373b9de138f
rosale <rosale@usi.ch>
2020-11-22 12:58:05 +0100

    Update TestExample.java
    Adding a stream to test the profiling of Collectors

--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -4,7 +4,14 @@ package test;
 import static org.junit.Assert.*;
 
 import org.junit.*;
+import java.util.Arrays;
 import example.Example;
+import java.util.Map;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class TestExample {
 
@@ -26,8 +33,16 @@ public class TestExample {
 
 	@Test
 	public void testSum() throws Exception {
-		String abc8 = "abcdefghi";
-	        abc8.chars().count();
+
+        List<String> g = Arrays.asList("geeks", "for", "geeks", "Alpha", "newer", "geeks"); 
+  
+        Map<String, Long> result 
+            = g.stream().collect( 
+                Collectors.groupingBy( 
+                    Function.identity(),
+                    () -> new TreeMap<String, Long>(Comparator.reverseOrder()),
+                    Collectors.counting())); 
+		
 		assertEquals(example.sum(), 55L);
 	}
 
