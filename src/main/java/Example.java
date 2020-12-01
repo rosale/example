50fbac1f968afa835ad84959f9a698d154272308
rosale <rosale@usi.ch>
2020-12-01 09:22:25 +0100

    Update Example.java
    Changes to test ThreadLocal(inheritable=true)

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,12 +9,24 @@ public class Example {
 	}
 	
 	public long sum() {
+		Thread t1 = new Thread(new MyRunnable(), "T1");
+                t1.start();
+		
 		IntStream is =  IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 		return is.sum();
+		
 	}
 	
 	public static void main(String args[]) {
 		System.out.println("Example.main - executed");
 	}
 	
+}
+
+class MyRunnable implements Runnable {
+            @Override
+            public void run() {
+                    List<String> g = Arrays.asList("geeks", "for", "geeks", "Alpha", "newer", "geeks");
+                    g.parallelStream().toArray();
+            }
 }
